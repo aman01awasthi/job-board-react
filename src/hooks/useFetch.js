08d9dbd80ca1b47b2e788afc6a7472bc435f6099
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-06-03 23:56:57 +0530

    Job board scaffold — routing, folder structure, base components

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,9 @@
+const useFetch = () => {
+
+    const [data, setdata] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+}
+
+export default useFetch;
