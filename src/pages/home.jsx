025bf0c69c0bb572ef3ded425bc85b209bdd1e18
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-07-10 00:03:09 +0530

    feat(pages): implement Home page with JobList component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,15 @@
+import { useJobs } from '../context/JobsContext';
+import JobList from '../components/JobList';
+
 const Home = () => {
-    <></>
-}
+  const { jobs } = useJobs();
+
+  return (
+    <div className="home-page">
+      <h1>Job Listings</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+};
 
 export default Home;
